ab44b57d10315028697f5214ecacbf73873f3ce9
asleepyseb <sebastian26ju@gmail.com>
2026-09-20 16:35:38 -0600

    Practica 01: Bandera de Mexico con Row y Box

---
+++ app/src/main/java/com/example/practicabanderas/BanderaMexico.kt
@@ -0,0 +1,11 @@
+package com.example.practicabanderas
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BanderaMexico(modifier: Modifier = Modifier){
+
+}
+class BanderaMexico {
+}
