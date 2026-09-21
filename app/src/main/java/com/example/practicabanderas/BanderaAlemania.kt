29af87c9c11eab84c249b1d9a1d9838048dbf721
asleepyseb <sebastian26ju@gmail.com>
2026-09-20 18:43:39 -0600

    bandera alemania creada

---
+++ app/src/main/java/com/example/practicabanderas/BanderaAlemania.kt
@@ -0,0 +1,43 @@
+package com.example.practicabanderas
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BanderaAlemania(modifier: Modifier = Modifier){
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color.Black)
+        )
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color(0xFFDD0000))
+        )
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color(0xFFFFCE00))
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BanderaAlemaniaPrevie() {
+    Surface {
+        BanderaAlemania(modifier = Modifier.fillMaxSize())
+    }
+}
